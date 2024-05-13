e1567c006debcdf2cfc613a6fb89103a5768257b
Alex Cholmsky <acholmsk@uwaterloo.ca>
2024-05-13 14:59:59 -0400

    http requests with example.com

--- Producer/producer.go
+++ Producer/producer.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-    endpoint   = flag.String("endpoint", "http://localhost:8080", "Endpoint to send requests to")
+    endpoint   = flag.String("endpoint", "http://example.com", "Endpoint to send requests to")
     mode       = flag.String("mode", "http", "Operating mode: 'http' or 'queue'")
     rate       = flag.Int("rate", 10, "Requests per second")
 )
@@ -22,16 +22,19 @@ func httpRequests() {
     consecutiveFailures := 0  // Counter for consecutive failures
 
     for range ticker.C {
-		_, err := http.Get(*endpoint)
-		if err != nil {
-			log.Printf("Failed to reach endpoint: %v\n", err)
-			consecutiveFailures++
-			if consecutiveFailures >= 10 {
-				log.Fatalf("Failed to reach endpoint 10 times in a row: %v\n", err)
-			}
-		} else {
-			consecutiveFailures = 0  // Reset the failure count on a successful request
-		}
+        resp, err := http.Get(*endpoint)
+        if err != nil {
+            log.Printf("Failed to reach endpoint: %v\n", err)
+            consecutiveFailures++
+            if consecutiveFailures >= 10 {
+                log.Fatalf("Failed to reach endpoint 10 times in a row: %v\n", err)
+            }
+        } else {
+            log.Printf("Successfully reached endpoint: %v, Status Code: %d\n", *endpoint, resp.StatusCode)
+            // Ensure body is closed after handling the response
+            resp.Body.Close()
+            consecutiveFailures = 0  // Reset the failure count on a successful request
+        }
     }
 }
 
@@ -42,8 +45,7 @@ func messageQueue(){
 func main() {
 	if *mode == "http" {
 		httpRequests()
-	}
-	else {
+	} else {
 		messageQueue()
 	}
 }
